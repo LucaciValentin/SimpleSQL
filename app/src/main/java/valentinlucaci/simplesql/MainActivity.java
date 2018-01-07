8760c0f2dd8b9cef616a35bbf23eda34c083ede7
LucaciValentin <lucacivalentinv@gmail.com>
2018-01-07 22:26:03 +0200

    probe

--- app/src/main/java/valentinlucaci/simplesql/MainActivity.java
+++ app/src/main/java/valentinlucaci/simplesql/MainActivity.java
@@ -2,12 +2,24 @@ package valentinlucaci.simplesql;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextName,editTextParola;
+    Button buttonSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextName = findViewById(R.id.editTextName);
+        editTextParola = findViewById(R.id.editTextPass);
+        buttonSave = findViewById(R.id.button);
+    }
+
+    public void saveData(View view) {
+        Toast.makeText(this, "db creat", Toast.LENGTH_SHORT).show();
     }
 }
