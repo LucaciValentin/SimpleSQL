f9e665e9d0774c3fe28913611c3fc7f774b76eea
LucaciValentin <lucacivalentinv@gmail.com>
2018-01-07 22:36:29 +0200

    probe

---
+++ app/src/main/java/valentinlucaci/simplesql/DataBase/Constant.java
@@ -0,0 +1,18 @@
+package valentinlucaci.simplesql.DataBase;
+
+
+import android.provider.BaseColumns;
+
+public class Constant {
+   public static final class ConstantAll implements BaseColumns{
+
+       //Table Name
+       public static final String TABLE_NAME ="data.db";
+       //Columns
+       public static final String _id = BaseColumns._ID;
+       public static final String COLUMNS_NAME = "columnName";
+       public static final String COLUMN_PASSOWORD = "columnPassword";
+       public static final String CreateTable = "";
+
+   }
+}
